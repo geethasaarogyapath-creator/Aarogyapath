fdd4696099a4fb5d28f076778eaf721dc09607a9
Raja Sai Datha Sushanth <rajasaidathasushanth2006@gmail.com>
2026-01-26 19:31:11 +0530

    l

--- controller/ClientController.js
+++ controller/ClientController.js
@@ -1,36 +1,37 @@
-const client = require('../models/Client')
-const express = require('express')
-const http = require("http");
-const { Server } = require("socket.io");
+const client = require('../models/Client');
 
-const register = async (req,res) => {
-    try {
-        const {name,email,phone,height,weight,proffession,age,gender,address,isvisited} = req.body;
-        const t = await client.findOne({email:email})
-        if(t){
-            res.status(200).json("Already Registered")
-        }else{
-            const c = new client({
-                name,
-                email,
-                phone,
-                height,
-                weight,
-                gender,
-                age,
-                proffession,
-                address,
-                isvisited
-            })
-            const user = await c.save()
-            req.io.emit("clientUpdated")
-            res.status(200).json("Registered Successfully")
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(400).json("Internal Server Error")
+const register = async (req, res) => {
+  try {
+    const { name, email, phone, height, weight, proffession, age, gender, address, isvisited } = req.body;
+
+    const t = await client.findOne({ email: email });
+    if (t) {
+      return res.status(200).json("Already Registered");
     }
-    
-}
 
-module.exports = {register} 
+    const c = new client({
+      name,
+      email,
+      phone,
+      height,
+      weight,
+      gender,
+      age,
+      proffession,
+      address,
+      isvisited
+    });
+
+    await c.save();
+
+    // Emit event to all connected frontends
+    req.io.emit("clientUpdated");
+
+    res.status(200).json("Registered Successfully");
+  } catch (error) {
+    console.log(error);
+    res.status(400).json("Internal Server Error");
+  }
+};
+
+module.exports = { register };
--- index.js
+++ index.js
@@ -1,58 +1,66 @@
-const express = require("express")
-const mongoose = require('mongoose')
-const clientrouter = require('./router/ClientRouter')
-const adminrouter = require('./router/AdminRouter')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const clientrouter = require("./router/ClientRouter");
+const adminrouter = require("./router/AdminRouter");
+const cors = require("cors");
+require("dotenv").config();
+
 const http = require("http");
 const { Server } = require("socket.io");
 
+const app = express();
+const server = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req,res,next)=>{
-    console.log(req.method,req.path);
-    req.io = io;
-    next()
-})
-
-
-app.get("/",(req,res)=>{
-    res.send("<p>HI its Backend</p>")
-})
-
 const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:5174',
-  'https://aarogyapath-frontend-admin.vercel.app',
-  'https://geethas-aarogyapath1.vercel.app',
-  'https://geethas-aarogyapath.vercel.app'
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://aarogyapath-frontend-admin.vercel.app",
+  "https://geethas-aarogyapath1.vercel.app",
+  "https://geethas-aarogyapath.vercel.app"
 ];
+
+const io = new Server(server, {
+  cors: { origin: allowedOrigins }
+});
+
+// Attach io to every request
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  req.io = io;
+  next();
+});
 
 app.use(cors({
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true
 }));
 
+app.get("/", (req, res) => {
+  res.send("<p>HI its Backend</p>");
+});
 
-app.use("/client",clientrouter);
-app.use("/admin",adminrouter)
+app.use("/client", clientrouter);
+app.use("/admin", adminrouter);
 
+io.on("connection", (socket) => {
+  console.log("Socket client connected");
+});
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
-    app.listen(process.env.PORT,()=>{
-        console.log("Mongo Connected and App Started")
-    })
-})
-.catch((error)=>{
-    console.log(error)
-})
+  .then(() => {
+    server.listen(process.env.PORT, () => {
+      console.log("Mongo Connected and App Started");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
